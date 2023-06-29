2187b5b39f49831ec0d6b25f4584e55407f6fa96
Mercure Mekinda <mercuremekinda@gmail.com>
2023-06-29 22:31:19 +0100

    build dashboard

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors())
 
+app.get('*',function (req, res) {
+    res.redirect('/');
+});
+
 // Start the app predictfoot
 app.listen(process.env.PORT || 3500, function() {
-    console.log("dashboard predicfoot live  started")
+    console.log("app portfolio mekinda mercure live  started")
 })
